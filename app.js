42a7ffa54f0034216c4df98cb73580b1725df70b
zhangruofan <ea7son@gmail.com>
2016-01-05 23:10:03 +0800

    fix

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var app =  require('koa')(),
     mount = require('mount-koa-routes')
     ;
 
-mount(app,__dirname+'/app/routes',true);
+mount(app,__dirname+'/app/routes');
 app.context.render=render({
     root:__dirname+'/app/views',
     autoescape: true,
-    cache: false, //'memory'
+    cache: 'memory',
     ext: 'html'
 });
 app.use(favicon(__dirname+"/public/img/logo/favicon.ico"));
