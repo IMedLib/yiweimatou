4b58a6839f0bf698cebc049e5d1e205582acd84e
zhangruofan <ea7son@gmail.com>
2016-01-22 13:16:23 +0800

    add test

--- configs/develop.js
+++ configs/develop.js
@@ -1,12 +1,12 @@
 module.exports={
     url:{
         inside: {
-            api: "http://121.41.92.56:8080/",
+            api: "http://121.41.92.56:8081/",
             upload: "http://121.41.92.56:8001/",
             domain: "http://121.41.92.56/"
         },
         outside: {
-            api: "http://121.41.92.56:8080/",
+            api: "http://121.41.92.56:8081/",
             upload: "http://121.41.92.56:8001/",
             domain: "http://121.41.92.56/"
         }
