7154a426b3c0cfce98d2bedc5f0bbff58d885922
zhangruofan <ea7son@gmail.com>
2016-03-07 09:53:34 +0800

    fix view error

--- config/_base.js
+++ config/_base.js
@@ -8,6 +8,6 @@ const config = {
   // Server Configuration
   // ----------------------------------
   server_host: 'localhost',
-  server_port: process.env.PORT || 3000
+  server_port: 3000
 }
 module.exports = config;
